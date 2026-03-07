3f65d756869f8f78ca973363a9ee886a875e28d6
XYZboom <xyzboom@qq.com>
2026-03-07 13:16:29 +0800

    feat: update figure size

--- src/reconstruct_c_source.py
+++ src/reconstruct_c_source.py
@@ -11,3 +11,20 @@ if __name__ == '__main__':
         engine_kwargs=dict(data_only=True)
     )
     draw(df)
+
+    df2 = pd.read_excel(
+        "reconstruct.xlsx",
+        header=[0, 1], nrows=18,
+        engine='openpyxl',
+        engine_kwargs=dict(data_only=True)
+    )
+    draw(df2, probs=['1.0', '0'], vert=False)
+
+    worse_than_perses = [3, 5, 7, 11, 12, 13, 16]
+    df_1 = pd.read_excel(
+        "reconstruct.xlsx",
+        header=[0, 1], nrows=18 - len(worse_than_perses), skiprows=worse_than_perses,
+        engine='openpyxl',
+        engine_kwargs=dict(data_only=True)
+    )
+    draw(df_1, probs=['1.0', '0'])
